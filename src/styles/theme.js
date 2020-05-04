14a54cc79b020ef2edcf23520227d7a411f4937a
Alley Hector <alleyhector@motivateco.com>
2020-05-04 14:16:42 -0700

    Fix colors

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,19 +1,19 @@
 import { hex2rgba } from '@utils';
 
-const ACCENT = '#64ffda';
-const DARK_BG = '#020c1b';
-const BG = '#0a192f';
+const ACCENT = '#7AA34B';
+const DARK_BG = '#273A52';
+const BG = '#273A52';
 
 const theme = {
   colors: {
     darkNavy: DARK_BG,
     navy: BG,
-    lightNavy: '#172a45',
-    lightestNavy: '#303C55',
-    slate: '#8892b0',
-    lightSlate: '#a8b2d1',
-    lightestSlate: '#ccd6f6',
-    white: '#e6f1ff',
+    lightNavy: '#2D435F',
+    lightestNavy: '#2D435F',
+    slate: '#cbb496',
+    lightSlate: '#cbb496',
+    lightestSlate: '#b49369',
+    white: '#FAF1CF',
     green: ACCENT,
     transGreen: hex2rgba(ACCENT, 0.07),
     shadowNavy: hex2rgba(DARK_BG, 0.7),
