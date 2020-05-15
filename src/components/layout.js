84060fd89adb8b2c435227e5229f02fb079e8548
Alley Hector <alleyhector@motivateco.com>
2020-05-14 21:46:37 -0700

    Sunset background with gradient nav bg

--- src/components/layout.js
+++ src/components/layout.js
@@ -57,7 +57,7 @@ const StyledContent = styled.div`
       top:0;
       z-index:-1;
       opacity:0.1;
-      background: url("./bg/pompidou.jpg") no-repeat fixed center center;
+      background: url("./bg/sunset.jpg") no-repeat fixed center center;
       background-size: cover;
       ${media.thone`
         opacity: 0;
