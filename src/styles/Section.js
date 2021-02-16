850bcd47a232599c431c6d0d1d99881a1454ac9f
Alley Hector <alleyhector@motivateco.com>
2021-02-16 10:23:33 -0800

    Make focus outline blend in

--- src/styles/Section.js
+++ src/styles/Section.js
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 import media from './media';
+import { theme, mixins } from '@styles';
+const { colors } = theme;
 
 const Section = styled.section`
   margin: 0 auto;
   padding: 150px 0;
   max-width: 1000px;
+  &:focus {
+    outline: ${colors.darkNavy};
+  }
 
   ${media.tablet`padding: 100px 0;`};
 `;
